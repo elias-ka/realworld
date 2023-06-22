88977f362b9be73b3b5a7fc686536935f752dbc9
elias-ka <48407293+elias-ka@users.noreply.github.com>
2023-06-22 13:43:45 +0300

    add logger

---
+++ realworld/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+from realworld.config import LOG_LEVEL
+
+LOG_FORMAT_DEBUG = (
+    "%(levelname)s:\t%(message)s:\n\t%(pathname)s:%(funcName)s:%(lineno)d"
+)
+
+level = getattr(logging, str(LOG_LEVEL).upper(), logging.WARNING)
+
+
+def configure_logging() -> None:
+    if level == logging.DEBUG:
+        logging.basicConfig(level=level, format=LOG_FORMAT_DEBUG)
+    else:
+        logging.basicConfig(level=level)
+
+
+logger = logging.getLogger("realworld")
