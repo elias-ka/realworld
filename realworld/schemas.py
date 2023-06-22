9e8514dc7269b11dace53933f51da10e5b75506f
elias-ka <48407293+elias-ka@users.noreply.github.com>
2023-06-22 13:47:08 +0300

    create a custom ``pydantic.BaseModel``

---
+++ realworld/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+
+def _snake_to_camel_case(string: str) -> str:
+    words = string.split("_")
+    return words[0] + "".join(word.capitalize() for word in words[1:])
+
+
+class RealWorldBaseModel(BaseModel):
+    class Config:
+        alias_generator = _snake_to_camel_case
